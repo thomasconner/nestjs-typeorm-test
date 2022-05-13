5a284d03763a61d14fd62359329a8f406d19b0c8
Thomas Conner <tconner@6river.com>
2022-05-13 07:30:34 -0400

    fix: destroy datasource in app e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { DataSource } from 'typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let dataSource: DataSource;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -13,6 +15,12 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    dataSource = app.get<DataSource>(DataSource);
+  });
+
+  afterEach(async function () {
+    await dataSource?.destroy();
+    await app?.close();
   });
 
   it('/ (GET)', () => {
